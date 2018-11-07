e91b0f3688adf9e67e2b02cb4b6d75b5eadabd37
FannyAndrade243 <fanny.andradec@ug.edu.ec>
2018-11-06 19:58:21 -0500

    Datos Generales (Curriculum Actualizado)

--- app/src/main/java/com/example/fanny/micurriculo/Main5Activity.java
+++ app/src/main/java/com/example/fanny/micurriculo/Main5Activity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class Main5Activity extends AppCompatActivity {
-    Button botonAtras4;
+Button botonAtras4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
